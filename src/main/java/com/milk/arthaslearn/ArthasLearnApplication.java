0048f07e4e258740910864a3ea11e4eeeb08e9c3
wh <2574021892@qq.com>
2023-04-17 17:57:23 +0800

    fix

--- src/main/java/com/milk/arthaslearn/ArthasLearnApplication.java
+++ src/main/java/com/milk/arthaslearn/ArthasLearnApplication.java
@@ -2,6 +2,11 @@ package com.milk.arthaslearn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
 
 @SpringBootApplication
 public class ArthasLearnApplication {
--- src/main/java/com/milk/arthaslearn/WatchController.java
+++ src/main/java/com/milk/arthaslearn/WatchController.java
@@ -1,19 +1,60 @@
 package com.milk.arthaslearn;
 
 import cn.hutool.core.thread.ThreadUtil;
+import cn.hutool.extra.spring.SpringUtil;
 import com.milk.arthaslearn.dto.User;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
 
 @Slf4j
 @RestController
 @RequestMapping(value = "watch")
+@RequiredArgsConstructor
 public class WatchController {
+
+    /**
+     * 核心线程数 = cpu 核心数 + 1
+     */
+    private static final int core = Runtime.getRuntime().availableProcessors() + 1;
+
+    public static Executor testExecutor;
+
+    @PostConstruct
+    public void sendTask() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        // 核心线程数
+        executor.setCorePoolSize(core);
+        // 最大线程数
+        executor.setMaxPoolSize(core * 2);
+        // 等待队列容量
+        executor.setQueueCapacity(200);
+        // 保活时间
+        executor.setKeepAliveSeconds(60);
+        // 线程名前缀
+        executor.setThreadNamePrefix("testExecutor-");
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        executor.initialize();
+        testExecutor = executor;
+
+        for (int i = 0; i < 2; i++) {
+            testExecutor.execute(() -> {
+                while (true) {
+                    ThreadUtil.sleep(5000);
+                    log.info("线程 {} 正在执行", Thread.currentThread().getName());
+                }
+            });
+        }
+    }
 
     @GetMapping(value = "test")
     public List<User> test(String method, String name2, @RequestBody List<User> userList) {
@@ -30,7 +71,7 @@ public class WatchController {
             StaticClass.changeName3(name2);
         }
         if ("traceJdk".equals(method)) {
-            System.out.println("sda".replace("s",""));
+            System.out.println("sda".replace("s", ""));
         }
         if ("deadLock".equals(method)) {
             deadLock();
@@ -46,7 +87,7 @@ public class WatchController {
         log.info("controller log 被调用");
     }
 
-    private void deadLock(){
+    private void deadLock() {
         Object lock1 = new Object();
         Object lock2 = new Object();
 
